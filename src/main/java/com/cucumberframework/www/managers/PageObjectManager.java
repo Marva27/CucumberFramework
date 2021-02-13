c3764a32b4a9bc82d2f236971477b008cac93083
vijay <vijay@VijayPC>
2021-02-13 12:09:23 -0500

    Reorganized the folder structure

--- src/main/java/com/cucumberframework/www/managers/PageObjectManager.java
+++ src/main/java/com/cucumberframework/www/managers/PageObjectManager.java
@@ -2,8 +2,6 @@ package com.cucumberframework.www.managers;
 
 import org.openqa.selenium.WebDriver;
 
-import com.cucumberframework.www.pages.linkedin.ExpediaLoginPage;
-import com.cucumberframework.www.pages.linkedin.ExpediaSearchPage;
 import com.cucumberframework.www.pages.linkedin.MercuryFlightLoginPage;
 
 
@@ -11,8 +9,7 @@ public class PageObjectManager {
 	
 	private WebDriver browser;
 	
-	public ExpediaLoginPage expediaLoginPage;
-	public ExpediaSearchPage expediaSearchPage;
+
 	public MercuryFlightLoginPage mercuryFlightLoginPage;
 	
 	
@@ -20,13 +17,6 @@ public class PageObjectManager {
 		this.browser = browser;
 	}
 	
-	public ExpediaLoginPage getExpediaLoginPage() {
-		return (expediaLoginPage == null) ? expediaLoginPage = new ExpediaLoginPage(browser) : expediaLoginPage;
-	}
-
-	public ExpediaSearchPage getExpediaSearchPage() {
-		return (expediaSearchPage == null) ? expediaSearchPage = new ExpediaSearchPage(browser) : expediaSearchPage;
-	}
 
 	public MercuryFlightLoginPage getMercuryFlightLoginPage() {
 		return (mercuryFlightLoginPage == null) ? mercuryFlightLoginPage = new MercuryFlightLoginPage(browser) : mercuryFlightLoginPage;
